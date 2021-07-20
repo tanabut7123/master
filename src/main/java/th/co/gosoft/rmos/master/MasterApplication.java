84f0a925fe422581f5eea3a234d17c8897f2a190
Tanabut Sivarut <tanabut7123@gmail.com>
2021-07-20 22:11:57 +0700

    print total beans

--- src/main/java/th/co/gosoft/rmos/master/MasterApplication.java
+++ src/main/java/th/co/gosoft/rmos/master/MasterApplication.java
@@ -2,12 +2,20 @@ package th.co.gosoft.rmos.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MasterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MasterApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(MasterApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		System.out.println(context.getBeanDefinitionCount());
+		for (String bean:beans) {
+			System.out.println(bean);
+		}
+
 	}
 
 }
