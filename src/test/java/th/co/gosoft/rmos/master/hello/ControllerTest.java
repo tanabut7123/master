1d29ae83676cd2fc48228583477bc1b33dc70362
Tanabut Sivarut <tanabut7123@gmail.com>
2021-07-24 22:47:40 +0700

    use test error entity

--- src/test/java/th/co/gosoft/rmos/master/hello/ControllerTest.java
+++ src/test/java/th/co/gosoft/rmos/master/hello/ControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,5 +21,32 @@ class ControllerTest {
         Response response = testRestTemplate.getForObject("/hello/tanabut", Response.class);
 
         assertEquals("tanabut", response.getMessage());
+    }
+
+    @Test
+    public void getHelloWithType1ShouldBeSizeOfResponseIs4() {
+        Response[] responses = testRestTemplate.getForObject("/hello?type=1", Response[].class);
+
+        assertEquals(4, responses.length);
+    }
+
+    @Test
+    public void getHelloWithType2ShouldBeSizeOfResponseIs7() {
+        Response[] responses = testRestTemplate.getForObject("/hello?type=2", Response[].class);
+
+        assertEquals(7, responses.length);
+    }
+
+    @Test
+    public void getHelloWithNotTypeShouldBeRestClientException() {
+        ResponseEntity<ErrorResponse> responseEntity = testRestTemplate.getForEntity("/hello", ErrorResponse.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("SYM00003", responseEntity.getBody().getErrorCode());
+        assertTrue(responseEntity.getBody().getErrorMessage().contains("'type'"));
+
+       /* assertThrows(RestClientException.class, () -> {
+            testRestTemplate.getForObject("/hello", Response[].class);
+        });*/
     }
 }
