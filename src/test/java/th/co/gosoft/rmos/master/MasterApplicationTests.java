af9ca0d0a10b41dd3aa137ef206d46b91b34d4fe
tanabut <tanabut7123@gmail.com>
2021-08-28 22:53:41 +0700

    test jenkins project auto build4

--- src/test/java/th/co/gosoft/rmos/master/MasterApplicationTests.java
+++ src/test/java/th/co/gosoft/rmos/master/MasterApplicationTests.java
@@ -3,14 +3,11 @@ package th.co.gosoft.rmos.master;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 @SpringBootTest
 class MasterApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertFalse(true);
 	}
 
 }
